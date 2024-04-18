41fc03bc1a58ef8a3fd60f8be19aac35912da02b
relcnob <f.relcnob@gmail.com>
2024-04-18 16:58:49 +0200

    fix: typo

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -3,7 +3,7 @@ import "./../styles/reset.css";
 import "./../styles/globals.css";
 import "./../styles/output.css";
 import Footer from "../components/ui-elements/footer/Footer";
-import Header from "../components/ui-elements/header/Header";
+import Header from "../components/ui-elements/header/header";
 import { ThemeProvider } from "next-themes";
 
 export const metadata: Metadata = {
